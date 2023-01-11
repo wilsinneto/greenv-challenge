40ba4ca96681a3854054ff60d265b95b5149b904
wilson Neto <wngaspar10@gmail.com>
2023-01-11 10:16:08 -0300

    feat: add User class

---
+++ src/entities/user.ts
@@ -0,0 +1,30 @@
+import { Email, Name, UserData } from '@/entities'
+import { InvalidEmailError, InvalidNameError } from '@/entities/errors'
+import { Either, left, right } from '@/shared'
+
+export class User {
+  public readonly email: Email
+  public readonly name: Name
+
+  private constructor (name: Name, email: Email) {
+    this.name = name
+    this.email = email
+  }
+
+  static create (userData: UserData): Either<InvalidNameError | InvalidEmailError, User> {
+    const nameOrError = Name.create(userData.name)
+    if (nameOrError.isLeft()) {
+      return left(nameOrError.value)
+    }
+
+    const emailOrError = Email.create(userData.email)
+    if (emailOrError.isLeft()) {
+      return left(emailOrError.value)
+    }
+
+    const name: Name = nameOrError.value as Name
+    const email: Email = emailOrError.value as Email
+
+    return right(new User(name, email))
+  }
+}
