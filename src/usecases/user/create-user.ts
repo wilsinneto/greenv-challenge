ebdb5e6aaf0c3dfcb2680f2a69678af14a8fdc50
wilson Neto <wngaspar10@gmail.com>
2023-01-11 10:46:53 -0300

    feat: add create user use case

---
+++ src/usecases/user/create-user.ts
@@ -0,0 +1,30 @@
+import { InvalidEmailError, InvalidNameError } from '@/entities/errors'
+
+import { User, UserData } from '@/entities'
+
+import { Either, left, right } from '@/shared'
+import { UseCase } from '@/usecases/ports'
+import { UserRepository } from '@/usecases/user/ports'
+
+export class CreateUser implements UseCase {
+  private readonly userRepo: UserRepository
+
+  constructor (userRepo: UserRepository) {
+    this.userRepo = userRepo
+  }
+
+  public async perform (request: UserData):
+    Promise<Either<InvalidNameError | InvalidEmailError, UserData>> {
+    const userOrError: Either<InvalidNameError | InvalidEmailError, User> = User.create(request)
+
+    if (userOrError.isLeft()) {
+      return left(userOrError.value)
+    }
+
+    if (!(await this.userRepo.exists(request))) {
+      await this.userRepo.add(request)
+    }
+
+    return right(request)
+  }
+}
