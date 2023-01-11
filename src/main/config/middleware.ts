73844b7998db6e3ae7cc23c15513274a02f83b3f
wilson Neto <wngaspar10@gmail.com>
2023-01-11 11:07:28 -0300

    feat: add body-parser, content-type and cors middleware files

---
+++ src/main/config/middleware.ts
@@ -0,0 +1,8 @@
+import { bodyParser, contentType, cors } from '@/main/middleware'
+import { Express } from 'express'
+
+export default (app: Express): void => {
+  app.use(bodyParser)
+  app.use(cors)
+  app.use(contentType)
+}
