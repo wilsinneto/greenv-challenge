270f9dec2decbfe9db1f7c056c5ff41fa6c90af4
wilson Neto <wngaspar10@gmail.com>
2023-01-11 10:16:44 -0300

    test: add User class

---
+++ test/entities/user.spec.ts
@@ -0,0 +1,38 @@
+import { User } from '@/entities'
+import { InvalidNameError } from '@/entities/errors'
+import { left } from '@/shared'
+
+describe('User domain entity', () => {
+  test('should not create user with invalid e-mail address', () => {
+    const invalidEmail = 'invalid_email'
+
+    const error = User.create({ name: 'any_name', email: invalidEmail }).value as Error
+
+    expect(error.name).toEqual('InvalidEmailError')
+    expect(error.message).toEqual('Invalid email: ' + invalidEmail + '.')
+  })
+
+  test('should not create user with invalid name (too few characters)', () => {
+    const invalidName = 'O        '
+
+    const error = User.create({ name: invalidName, email: 'any@mail.com' }).value as Error
+
+    expect(error.name).toEqual('InvalidNameError')
+  })
+
+  test('should not create user with invalid name (too many characters)', () => {
+    const invalidName = 'O'.repeat(257)
+    const error = User.create({ name: invalidName, email: 'any@mail.com' })
+    expect(error).toEqual(left(new InvalidNameError(invalidName)))
+  })
+
+  test('should create user with valid data', () => {
+    const validName = 'any_name'
+    const validEmail = 'any@mail.com'
+
+    const user: User = User.create({ name: validName, email: validEmail }).value as User
+
+    expect(user.name.value).toEqual(validName)
+    expect(user.email.value).toEqual(validEmail)
+  })
+})
