dab743b1f51c57082706b40d3e19176010b4787a
wilson Neto <wngaspar10@gmail.com>
2023-01-11 10:51:07 -0300

    feat: add in memory user repository

---
+++ src/usecases/user/repository/in-memory-user-repository.ts
@@ -0,0 +1,36 @@
+import { UserData } from '@/entities'
+import { UserRepository } from '@/usecases/user/ports'
+
+export class InMemoryUserRepository implements UserRepository {
+  private repository: UserData[]
+
+  constructor (repository: UserData[]) {
+    this.repository = repository
+  }
+
+  async add (user: UserData): Promise<void> {
+    const exists = await this.exists(user)
+
+    if (!exists) {
+      this.repository.push(user)
+    }
+  }
+
+  async findUserByEmail (email: string): Promise<UserData> {
+    const user = this.repository.find(user => user.email === email)
+
+    return user || null
+  }
+
+  async findAllUsers (): Promise<UserData[]> {
+    return this.repository
+  }
+
+  async exists (user: UserData): Promise<boolean> {
+    if (await this.findUserByEmail(user.email) === null) {
+      return false
+    }
+
+    return true
+  }
+}
