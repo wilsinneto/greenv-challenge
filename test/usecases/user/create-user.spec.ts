9e33fa07944cef589af942a5d4ff8c9e405fd338
wilson Neto <wngaspar10@gmail.com>
2023-01-11 13:11:10 -0300

    test: create user use case

---
+++ test/usecases/user/create-user.spec.ts
@@ -0,0 +1,48 @@
+import { UserData } from '@/entities'
+import { CreateUser } from '@/usecases/user'
+import { UserRepository } from '@/usecases/user/ports'
+import { InMemoryUserRepository } from '@/usecases/user/repository'
+
+describe('Create user use case', () => {
+  test('should add user with complete data', async () => {
+    const users: UserData[] = []
+    const repo: UserRepository = new InMemoryUserRepository(users)
+    const useCase: CreateUser = new CreateUser(repo)
+    const name = 'any_name'
+    const email = 'any@email.com'
+
+    const response = await useCase.perform({ name, email })
+    const user = repo.findUserByEmail('any@email.com')
+
+    expect((await user).name).toBe('any_name')
+    expect(response.value.name).toBe('any_name')
+  })
+
+  test('should not add user with invalid email', async () => {
+    const users: UserData[] = []
+    const repo: UserRepository = new InMemoryUserRepository(users)
+    const useCase: CreateUser = new CreateUser(repo)
+    const name = 'any_name'
+    const invalidEmail = 'invalid_email'
+
+    const response = (await useCase.perform({ name, email: invalidEmail })).value as Error
+    const user = await repo.findUserByEmail(invalidEmail)
+
+    expect(user).toBeNull()
+    expect(response.name).toEqual('InvalidEmailError')
+  })
+
+  test('should not add user with invalid name', async () => {
+    const users: UserData[] = []
+    const repo: UserRepository = new InMemoryUserRepository(users)
+    const useCase: CreateUser = new CreateUser(repo)
+    const invalidName = ''
+    const email = 'any@email.com'
+
+    const response = (await useCase.perform({ name: invalidName, email })).value as Error
+    const user = await repo.findUserByEmail(email)
+
+    expect(user).toBeNull()
+    expect(response.name).toEqual('InvalidNameError')
+  })
+})
