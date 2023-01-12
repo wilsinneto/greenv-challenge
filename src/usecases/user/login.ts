333a6071461b6430fcbfa18066fcf02e2d16c6a4
wilson Neto <wngaspar10@gmail.com>
2023-01-12 14:30:12 -0300

    feat: add login use case

---
+++ src/usecases/user/login.ts
@@ -0,0 +1,34 @@
+import { Password } from '@/entities'
+
+import { InvalidEmailOrPasswordError } from '@/entities/errors/invalid-email-password-error'
+import { Either, left, right } from '@/shared'
+import { UseCase } from '@/usecases/ports'
+import { UserRepository } from '@/usecases/user/ports'
+
+export interface LoginData {
+  email: string,
+  password: string
+}
+
+export class Login implements UseCase {
+  private readonly userRepo: UserRepository
+
+  constructor (userRepo: UserRepository) {
+    this.userRepo = userRepo
+  }
+
+  public async perform (request: LoginData):
+    Promise<Either<InvalidEmailOrPasswordError, LoginData>> {
+    const userOrNull = await this.userRepo.findUserByEmail(request.email)
+
+    if (!userOrNull) {
+      return left(new InvalidEmailOrPasswordError())
+    }
+
+    if (userOrNull && (Password.decrypt(userOrNull.password) !== request.password)) {
+      return left(new InvalidEmailOrPasswordError())
+    }
+
+    return right(request)
+  }
+}
