fb1ef7eed78ee28e6cdc02a21e0684045af3419f
wilson Neto <wngaspar10@gmail.com>
2023-01-12 15:23:31 -0300

    feat: add login controller

---
+++ src/web-controllers/login-controller.ts
@@ -0,0 +1,37 @@
+import { UserData } from '@/entities'
+import { UseCase } from '@/usecases/ports'
+import { HttpRequest, HttpResponse } from '@/web-controllers/ports'
+import { badRequest, created, serverError } from '@/web-controllers/util/http-helper'
+import { MissingParamError } from './errors/missing-param-error'
+
+export class LoginController {
+  private readonly usecase: UseCase
+
+  constructor (usecase: UseCase) {
+    this.usecase = usecase
+  }
+
+  public async handle (request: HttpRequest): Promise<HttpResponse> {
+    try {
+      if (!(request.body.email) || !(request.body.password)) {
+        let missingParam = !(request.body.email) ? 'email ' : ''
+        missingParam += !(request.body.password) ? 'password' : ''
+
+        return badRequest(new MissingParamError(missingParam.trim()))
+      }
+
+      const userData: UserData = request.body
+      const response = await this.usecase.perform(userData)
+
+      if (response.isLeft()) {
+        return badRequest(response.value)
+      }
+
+      if (response.isRight()) {
+        return created({ email: response.value.email })
+      }
+    } catch (error) {
+      return serverError(error)
+    }
+  }
+}
