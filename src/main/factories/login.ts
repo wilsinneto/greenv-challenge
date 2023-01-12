88486cf14ba8d6967a334bd2e239eac15f7bd684
wilson Neto <wngaspar10@gmail.com>
2023-01-12 15:24:20 -0300

    feat: create factory login

---
+++ src/main/factories/login.ts
@@ -0,0 +1,12 @@
+import { Login } from '@/usecases/user/login'
+import { InMemoryUserRepository } from '@/usecases/user/repository'
+import { LoginController } from '@/web-controllers/login-controller'
+import { database } from './database'
+
+export const makeLoginController = (): LoginController => {
+  const inMemoryUserRepository = new InMemoryUserRepository(database)
+  const createUserUseCase = new Login(inMemoryUserRepository)
+  const createUserController = new LoginController(createUserUseCase)
+
+  return createUserController
+}
