1cc0df890b574b10d2371e85ead044fb0c8df5bf
wilson Neto <wngaspar10@gmail.com>
2023-01-12 14:28:53 -0300

    feat: create unique database

--- src/main/factories/create-user.ts
+++ src/main/factories/create-user.ts
@@ -1,9 +1,10 @@
 import { CreateUser } from '@/usecases/user'
 import { InMemoryUserRepository } from '@/usecases/user/repository'
 import { CreateUserController } from '@/web-controllers/'
+import { database } from './database'
 
 export const makeCreateUserController = (): CreateUserController => {
-  const inMemoryUserRepository = new InMemoryUserRepository([])
+  const inMemoryUserRepository = new InMemoryUserRepository(database)
   const createUserUseCase = new CreateUser(inMemoryUserRepository)
   const createUserController = new CreateUserController(createUserUseCase)
 
