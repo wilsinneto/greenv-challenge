fbf190a6400ee959ca4364a745d86599a6db4b1a
wilson Neto <wngaspar10@gmail.com>
2023-01-12 14:30:23 -0300

    test: add login use case

---
+++ test/usecases/user/login.spec.ts
@@ -0,0 +1,49 @@
+import { UserData } from '@/entities'
+import { CreateUser } from '@/usecases/user'
+import { Login } from '@/usecases/user/login'
+import { UserRepository } from '@/usecases/user/ports'
+import { InMemoryUserRepository } from '@/usecases/user/repository'
+
+describe('Login use case', () => {
+  test('should login with the existing user', async () => {
+    const users: UserData[] = []
+    const repo: UserRepository = new InMemoryUserRepository(users)
+    const createUserUseCase: CreateUser = new CreateUser(repo)
+    const loginUseCase: Login = new Login(repo)
+    const name = 'any_name'
+    const email = 'any@email.com'
+    const password = 'abc'
+
+    await createUserUseCase.perform({ name, email, password })
+    const user = loginUseCase.perform({ email, password })
+
+    expect((await user).value).toEqual({ email, password })
+  })
+
+  test('should not login with no-existing user', async () => {
+    const users: UserData[] = []
+    const repo: UserRepository = new InMemoryUserRepository(users)
+    const useCase: Login = new Login(repo)
+    const email = 'any@mail.com'
+    const password = 'abc'
+
+    const response = (await useCase.perform({ email, password })).value as Error
+
+    expect(response.name).toEqual('InvalidEmailOrPasswordError')
+  })
+
+  test('should not login with invalid password', async () => {
+    const users: UserData[] = []
+    const repo: UserRepository = new InMemoryUserRepository(users)
+    const createUserUseCase: CreateUser = new CreateUser(repo)
+    const loginUseCase: Login = new Login(repo)
+    const name = 'any_name'
+    const email = 'any@email.com'
+    const password = 'abc'
+
+    await createUserUseCase.perform({ name, email, password })
+    const response = (await loginUseCase.perform({ email, password: 'cba' })).value as Error
+
+    expect(response.name).toEqual('InvalidEmailOrPasswordError')
+  })
+})
