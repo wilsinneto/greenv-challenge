ab21e8368792b695a50fd444759b1544bcfd7e2e
wilson Neto <wngaspar10@gmail.com>
2023-01-12 15:55:10 -0300

    refactor: updated imports

--- src/main/factories/login.ts
+++ src/main/factories/login.ts
@@ -1,7 +1,7 @@
+import { database } from '@/main/database'
 import { Login } from '@/usecases/user/login'
 import { InMemoryUserRepository } from '@/usecases/user/repository'
-import { LoginController } from '@/web-controllers/login-controller'
-import { database } from '../database'
+import { LoginController } from '@/web-controllers'
 
 export const makeLoginController = (): LoginController => {
   const inMemoryUserRepository = new InMemoryUserRepository(database)
