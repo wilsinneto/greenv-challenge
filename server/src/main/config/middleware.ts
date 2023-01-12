0b65050fc7d6265d9df301b6e0f0c185d8e5f7ae
wilson Neto <wngaspar10@gmail.com>
2023-01-12 19:58:05 -0300

    feat: added header safe with helmet component

--- server/src/main/config/middleware.ts
+++ server/src/main/config/middleware.ts
@@ -1,8 +1,10 @@
 import { bodyParser, contentType, cors } from '@/main/middleware'
 import { Express } from 'express'
+import helmet from 'helmet'
 
 export default (app: Express): void => {
   app.use(bodyParser)
   app.use(cors)
   app.use(contentType)
+  app.use(helmet())
 }
