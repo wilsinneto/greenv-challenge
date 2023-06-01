aa2bca6f046f23a7eccd80575e50524e48aac88c
Wilson Neto <wngaspar10@gmail.com>
2023-05-31 22:19:33 -0300

    test: add object value Phone

---
+++ server/test/entities/phone-validation.spec.ts
@@ -0,0 +1,33 @@
+import { Phone } from '@/entities'
+
+describe('Phone validation', () => {
+  test('should not accept null strings', () => {
+    const phone: null = null
+    expect(Phone.validate(phone)).toBeFalsy()
+  })
+
+  test('should not accept empty strings', () => {
+    const phone: string = ''
+    expect(Phone.validate(phone)).toBeFalsy()
+  })
+
+  test('should not accept phone with letters', () => {
+    const phone: string = '(mm)abusa-abcd'
+    expect(Phone.validate(phone)).toBeFalsy()
+  })
+
+  test('should not accept phone less than 11 numbers', () => {
+    const phone: string = '(11)99000-377'
+    expect(Phone.validate(phone)).toBeFalsy()
+  })
+
+  test('should not accept phone greater than 11 numbers', () => {
+    const phone: string = '(11)99000-37772'
+    expect(Phone.validate(phone)).toBeFalsy()
+  })
+
+  test('should accept valid phone', () => {
+    const phone: string = '(11)99000-3777'
+    expect(Phone.validate(phone)).toBeTruthy()
+  })
+})
