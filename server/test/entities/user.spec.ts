4430f7c6705e9dbc3d6b025b7fccb00395d4209e
Wilson Neto <wngaspar10@gmail.com>
2023-05-31 22:20:32 -0300

    test: add object value Phone

--- server/test/entities/user.spec.ts
+++ server/test/entities/user.spec.ts
@@ -6,7 +6,7 @@ describe('User domain entity', () => {
   test('should not create user with invalid e-mail address', () => {
     const invalidEmail = 'invalid_email'
 
-    const error = User.create({ name: 'any_name', email: invalidEmail, password: 'abc', cpf: '896.987.609-03' }).value as Error
+    const error = User.create({ name: 'any_name', email: invalidEmail, password: 'abc', cpf: '896.987.609-03', phone: '(11)99000-3777' }).value as Error
 
     expect(error.name).toEqual('InvalidEmailError')
     expect(error.message).toEqual('Invalid email: ' + invalidEmail + '.')
@@ -15,7 +15,7 @@ describe('User domain entity', () => {
   test('should not create user with invalid password', () => {
     const invalidPassword = 'a'
 
-    const error = User.create({ name: 'any_name', email: 'any@mail.com', password: invalidPassword, cpf: '896.987.609-03' }).value as Error
+    const error = User.create({ name: 'any_name', email: 'any@mail.com', password: invalidPassword, cpf: '896.987.609-03', phone: '(11)99000-3777' }).value as Error
 
     expect(error.name).toEqual('InvalidPasswordError')
     expect(error.message).toEqual('Invalid password: ' + invalidPassword + '.')
@@ -24,7 +24,7 @@ describe('User domain entity', () => {
   test('should not create user with invalid name (too few characters)', () => {
     const invalidName = 'O        '
 
-    const error = User.create({ name: invalidName, email: 'any@mail.com', password: 'abc', cpf: '896.987.609-03' }).value as Error
+    const error = User.create({ name: invalidName, email: 'any@mail.com', password: 'abc', cpf: '896.987.609-03', phone: '(11)99000-3777' }).value as Error
 
     expect(error.name).toEqual('InvalidNameError')
   })
@@ -32,7 +32,7 @@ describe('User domain entity', () => {
   test('should not create user with invalid name (too many characters)', () => {
     const invalidName = 'O'.repeat(257)
 
-    const error = User.create({ name: invalidName, email: 'any@mail.com', password: 'abc', cpf: '896.987.609-03' })
+    const error = User.create({ name: invalidName, email: 'any@mail.com', password: 'abc', cpf: '896.987.609-03', phone: '(11)99000-3777' })
 
     expect(error).toEqual(left(new InvalidNameError(invalidName)))
   })
@@ -40,10 +40,19 @@ describe('User domain entity', () => {
   test('should not create user with invalid cpf', () => {
     const invalidCpf = '000.000.000-00'
 
-    const error = User.create({ name: 'any_name', email: 'any@mail.com', password: 'abc', cpf: invalidCpf }).value as Error
+    const error = User.create({ name: 'any_name', email: 'any@mail.com', password: 'abc', cpf: invalidCpf, phone: '(11)99000-3777' }).value as Error
 
     expect(error.name).toEqual('InvalidCpfError')
     expect(error.message).toEqual('Invalid cpf: ' + invalidCpf + '.')
+  })
+
+  test('should not create user with invalid cpf', () => {
+    const invalidPhone = '(11)99000-377'
+
+    const error = User.create({ name: 'any_name', email: 'any@mail.com', password: 'abc', cpf: '896.987.609-03', phone: invalidPhone }).value as Error
+
+    expect(error.name).toEqual('InvalidPhoneError')
+    expect(error.message).toEqual('Invalid phone: ' + invalidPhone + '.')
   })
 
   test('should create user with valid data', () => {
@@ -51,8 +60,9 @@ describe('User domain entity', () => {
     const validEmail = 'any@mail.com'
     const validPassword = 'abc'
     const validCpf = '896.987.609-03'
+    const validPhone = '(11)99000-3777'
 
-    const user: User = User.create({ name: validName, email: validEmail, password: validPassword, cpf: validCpf }).value as User
+    const user: User = User.create({ name: validName, email: validEmail, password: validPassword, cpf: validCpf, phone: validPhone }).value as User
 
     expect(user.name.value).toEqual(validName)
     expect(user.email.value).toEqual(validEmail)
