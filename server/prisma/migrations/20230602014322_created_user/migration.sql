298e4f260ee6f8e9bbfa8a60a7a1f91cdf34bcf2
Wilson Neto <wngaspar10@gmail.com>
2023-06-01 22:53:14 -0300

    feat: add create user migration

---
+++ server/prisma/migrations/20230602014322_created_user/migration.sql
@@ -0,0 +1,14 @@
+-- CreateTable
+CREATE TABLE "Users" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
+    "cpf" TEXT NOT NULL,
+    "phone" TEXT NOT NULL,
+
+    CONSTRAINT "Users_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Users_email_key" ON "Users"("email");
