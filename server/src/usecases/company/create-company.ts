e7b2db76985ef20dda838375e1805f20d51a4d7d
Wilson Neto <wngaspar10@gmail.com>
2023-06-02 00:03:27 -0300

    feat: add create company

---
+++ server/src/usecases/company/create-company.ts
@@ -0,0 +1,28 @@
+import { InvalidCnpjError, InvalidEmailError, InvalidNameError, InvalidPhoneError } from '@/entities/errors'
+import { Company, CompanyData } from '@/entities/company'
+import { Either, left, right } from '@/shared'
+import { UseCase } from '@/usecases/ports'
+import { CompanyRepository } from '@/usecases/company/ports'
+
+export class CreateUser implements UseCase {
+  private readonly companyRepo: CompanyRepository
+
+  constructor (userRepo: CompanyRepository) {
+    this.companyRepo = userRepo
+  }
+
+  public async perform (request: CompanyData):
+    Promise<Either<InvalidNameError | InvalidEmailError | InvalidCnpjError | InvalidPhoneError, CompanyData>> {
+    const companyOrError: Either<InvalidNameError | InvalidEmailError | InvalidCnpjError | InvalidPhoneError, Company> = Company.create(request)
+
+    if (companyOrError.isLeft()) {
+      return left(companyOrError.value)
+    }
+
+    if (!(await this.companyRepo.exists(request))) {
+      await this.companyRepo.add(request)
+    }
+
+    return right({ id: companyOrError.value.id, ...request })
+  }
+}
