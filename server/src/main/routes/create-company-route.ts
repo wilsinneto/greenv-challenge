e78115ef65d7e6f47f8e5be7efd71031ce221260
wilson Neto <wngaspar10@gmail.com>
2023-06-03 02:29:57 -0300

    feat: add middleware authentication

--- server/src/main/routes/create-company-route.ts
+++ server/src/main/routes/create-company-route.ts
@@ -1,7 +1,8 @@
 import { adaptCreateCompanyRoute } from '@/main/adapters'
 import { makeCreateCompanyController } from '@/main/factories'
+import { authentication } from '@/main/middleware/authentication'
 import { Router } from 'express'
 
 export default (router: Router): void => {
-  router.post('/company', adaptCreateCompanyRoute(makeCreateCompanyController()))
+  router.post('/company', authentication, adaptCreateCompanyRoute(makeCreateCompanyController()))
 }
